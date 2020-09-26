86463d5f2515281458830f4e047cb922c62fadce
Toby Courtis <toby.courtis@hotmail.com>
2020-09-26 20:07:32 +0100

    Initial Commit

---
+++ black-bars/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  black-bars
+//
+//  Created by Toby Courtis on 26/09/2020.
+//  Copyright © 2020 Toby Courtis. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
